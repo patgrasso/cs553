77cabd02c20e029b643406bc2057965dc2d13898
nicholas antonov <nicholasantonov@gmail.com>
2016-10-13 23:27:21 -0400

    add comments

--- cluster.py
+++ cluster.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# Nicholas Antonov and Pat Grasso worked together on this assignment
+
 import nltk
 import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -37,12 +39,10 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(train_set)
 
 print(tfidf_matrix.shape)
 
+# run k means with 5 clusters
 num_clusters = 5
-
 km = KMeans(n_clusters=num_clusters)
-
 km.fit(tfidf_matrix)
-
 clusters = km.labels_.tolist()
 
 # print the first 10 documents with the cluster they belong to
@@ -59,7 +59,39 @@ for i in range(len(clusters)):
     ))
 
 
+# Print how many documents belong in each cluster and how many of them were serious incidents
 for key in cluster_dict:
     print("Cluster {}".format(key))
     print("Size:", len(cluster_dict[key]))
     print("# serious:", sum([doc[1] == "Y" for doc in cluster_dict[key]]))
+
+
+# do heirarchical clustering
+# was unable to get this working in the end, possibly the sample size was too large
+
+# import matplotlib.pyplot as plt
+# from sklearn.metrics.pairwise import cosine_similarity
+
+# dist = 1 - cosine_similarity(tfidf_matrix)
+# print('dist calculated')
+
+# from scipy.cluster.hierarchy import ward, dendrogram
+
+# linkage_matrix = ward(dist) #define the linkage_matrix using ward clustering pre-computed distances
+# print('warded')
+
+# fig, ax = plt.subplots(figsize=(15, 20)) # set size
+# ax = dendrogram(linkage_matrix, orientation="right", labels=titles);
+
+# plt.tick_params(\
+#     axis= 'x',          # changes apply to the x-axis
+#     which='both',      # both major and minor ticks are affected
+#     bottom='off',      # ticks along the bottom edge are off
+#     top='off',         # ticks along the top edge are off
+#     labelbottom='off')
+
+# plt.savefig('ward_clusters.png', dpi=200) #save figure as ward_clusters
+# print('saved')
+
+# plt.tight_layout() #show plot with tight layout
+
