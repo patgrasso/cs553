b5f8258de431d75824a1104adeb79b289d2237f8
Pat Grasso <pgrasso@stevens.edu>
2016-12-16 16:17:45 -0500

    Use pandas to load and manage data; shorten stuff
    Use the DecisionTreeClassifier from scikit and train on the bag of words
    matrix for the data. This does terribly, but...well we'll see.

--- assign5/decide.py
+++ assign5/decide.py
@@ -2,60 +2,47 @@
 
 # Nicholas Antonov and Pat Grasso worked together on this assignment
 
-import nltk
-import csv
-import math
-import re
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans, MiniBatchKMeans
+import nltk, csv, math, re, pandas
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.tree import DecisionTreeClassifier
 
-input = csv.reader(open("serious.csv", encoding="latin1"))
-headers = next(input)
+samples = pandas.read_csv("serious.csv", encoding="latin1")
+samples = samples[samples["SYMPTOM_TEXT"].notnull()]
 
-# read in the text from the csv
-text = []
-seriouses = []
+# Partition the data into training/testing sets
+train_samples = samples.sample(frac=0.5)
+test_samples = samples.drop(train_samples.index)
 
-samples = []
+print(train_samples.shape)
 
-for line in input:
-    samples += [(line[8], line[-1])]
-    text.append(line[8])
-    seriouses.append(line)
+#tfidf_vectorizer = TfidfVectorizer(max_df=0.9, max_features=200000,
+#                                 min_df=0.1, stop_words='english',
+#                                 use_idf=True, ngram_range=(1,3))
+#
+#tfidf_matrix = tfidf_vectorizer.fit_transform(train_samples["SYMPTOM_TEXT"])
+vectorizer = CountVectorizer(stop_words="english", ngram_range=(1,3),
+                             max_features=1000)
+word_matrix = vectorizer.fit_transform(train_samples["SYMPTOM_TEXT"]).toarray()
+test_matrix = vectorizer.fit_transform(test_samples["SYMPTOM_TEXT"]).toarray()
+print(word_matrix.shape)
+#features = tfidf_vectorizer.get_feature_names()
+#features = count_vectorizer.
 
-# Partition the data into the 3 sets
-test_size = int(len(text) * 0.7) # 10%
-validate_size = int(len(text) * 0.2) # 10%
+#symptoms = map(lambda x: x[0], train_samples)
+#m = map(lambda x: re.sub(r"[.,;]", "", x).split(' '), symptoms)
+#words = [word for doc in m for word in doc]
+#histogram = nltk.FreqDist(words)
 
-test_set = text[:test_size]
-validate_set = text[test_size:test_size + validate_size]
-train_set = text[test_size + validate_size:]
+#print(sorted(histogram.items(), key=lambda x:x[1], reverse=True)[:10])
 
-test_serious_set = seriouses[:test_size]
-validate_serious_set = seriouses[test_size:test_size + validate_size]
-train_serious_set = seriouses[test_size + validate_size:]
+print("Training Decision Tree Classifier...")
+#print(train_samples[0])
+#classifier = nltk.DecisionTreeClassifier.train(train_samples)
+#print(classifier.pseudocode(depth=4))
+clf = DecisionTreeClassifier(max_depth=30)
 
-test_samples = samples[:test_size]
-validate_samples = samples[test_size:test_size + validate_size]
-train_samples = samples[test_size + validate_size:]
-
-tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
-                                 min_df=0.2, stop_words='english',
-                                 use_idf=True, ngram_range=(1,3))
-
-tfidf_matrix = tfidf_vectorizer.fit_transform(train_set)
-
-symptoms = map(lambda x: x[0], train_samples)
-m = map(lambda x: re.sub(r"[.,;]", "", x).split(' '), symptoms)
-words = [word for doc in m for word in doc]
-histogram = nltk.FreqDist(words)
-
-print(sorted(histogram.items(), key=lambda x:x[1], reverse=True)[:10])
-
-exit(0)
-
-classifier = nltk.DecisionTreeClassifier.train(train_samples)
-print(classifier.pseudocode(depth=4))
+clf.fit(word_matrix, train_samples["SERIOUS"])
+print("score:", clf.score(test_matrix, test_samples["SERIOUS"]))
 
 
 
