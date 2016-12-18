87bd103f9ead1f4539c7b905ffa7711b64377d2e
patgrasso <pgrasso@stevens.edu>
2016-12-18 15:03:30 -0500

    Complete LDA topic extraction and classification
    Train some classifier on the topic likelihood matrix (with documents =
    rows, topics = columns). Right now it's a random forest, but that
    should be experimented with.
    
    Make a few small changes in result metrics in decide.py.

--- assign5/decide.py
+++ assign5/decide.py
@@ -2,7 +2,7 @@
 
 # Nicholas Antonov and Pat Grasso worked together on this assignment
 
-import nltk, csv, math, re, pandas, scipy
+import nltk, csv, math, re, pandas, scipy, string
 import numpy as np
 import warnings
 
@@ -12,7 +12,9 @@ from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
 from tficf import TficfVectorizer
+from nltk.stem.porter import PorterStemmer
 
 from printtree import tree_to_code
 
@@ -33,6 +35,25 @@ def report(results, n_top=3):
             print("Parameters: {0}".format(results["params"][candidate]))
             print()
 
+def tokenize(dataframe, column_label=""):
+    """
+    Filter stop words and punctuation
+    """
+    series = dataframe
+    if type(dataframe) != pandas.Series:
+        series = dataframe[column_label]
+
+    stop_words = nltk.corpus.stopwords.words("english") +\
+                 [sym for sym in string.punctuation]
+
+    remove_stops = lambda sent: [token for token in
+                                 nltk.word_tokenize(sent.lower())
+                                 if token not in stop_words]
+    stemmer = PorterStemmer()
+    stem_tokens = lambda tokens: [stemmer.stem(token) for token in tokens]
+
+    return dataframe[column_label].apply(remove_stops).apply(stem_tokens)
+
 
 samples = pandas.read_csv("serious.csv", encoding="latin1")
 samples = samples[samples["SYMPTOM_TEXT"].notnull()]
@@ -44,7 +65,12 @@ test_samples = samples.drop(train_samples.index)#.sample(frac=0.05)
 #test_no = test_samples[test_samples["SERIOUS"] == "N"].sample(test_yes.shape[0])
 #test_samples = pandas.concat((test_no, test_yes))
 
-print("Training on {} samples".format(train_samples.shape[0]))
+print("Train : {} ({:.2%})".format(
+    train_samples.shape[0],
+    train_samples.shape[0] / samples.shape[0]))
+print("Test  : {} ({:.2%})".format(
+    test_samples.shape[0],
+    test_samples.shape[0] / samples.shape[0]))
 
 vectorizers = {
     "tfidf": TfidfVectorizer(max_df=0.99, max_features=1000,
@@ -61,6 +87,14 @@ vectorizers = {
 
 # Pick vectorization method
 vectorizer = vectorizers["tfidf"]
+
+
+# Filter stop words and punctuation
+print("Removing stop words/stemming")
+train_samples["SYMPTOM_TEXT"] = tokenize(train_samples, "SYMPTOM_TEXT")\
+                                .apply(lambda x: ' '.join(x))
+test_samples["SYMPTOM_TEXT"] = tokenize(test_samples, "SYMPTOM_TEXT")\
+                               .apply(lambda x: ' '.join(x))
 
 print("Vectorizing with {}".format(vectorizer.__class__.__name__))
 
@@ -82,12 +116,14 @@ if isinstance(word_matrix, scipy.sparse.csr_matrix):
 
 print("Vectorization complete. Classifying...")
 
+
 # Set up decision tree
 clf = DecisionTreeClassifier(max_depth=5, min_samples_leaf=6)
 baseline = DummyClassifier(strategy="prior")
 
 clf.fit(word_matrix, train_samples["SERIOUS"])
 baseline.fit(word_matrix, train_samples["SERIOUS"])
+
 
 # Print tree
 #tree_to_code(clf.best_estimator_, vectorizer.get_feature_names())
@@ -98,20 +134,32 @@ export_graphviz(clf, out_file = "tree.dot",
                 impurity = False,
                 proportion = True)
 
+
 # Get feature importances
 feature_importances = sorted(zip(
     clf.feature_importances_,
     vectorizer.get_feature_names()
 ), reverse=True)
 
+
 # Print the top N important features
 for importance, feature in feature_importances[:10]:
     feature = (feature[:27] + "...") if len(feature) > 30 else feature
     print("{:30s}: {}".format(feature, importance))
 
+
 # Print classifier's accuracy on the test set
-print("score   :", clf.score(test_matrix, test_samples["SERIOUS"]))
-print("baseline:", baseline.score(test_matrix, test_samples["SERIOUS"]))
-
-
+print()
+print("score    :", clf.score(test_matrix, test_samples["SERIOUS"]))
+print("baseline :", baseline.score(test_matrix, test_samples["SERIOUS"]))
+print()
+print("~ confusion ~")
+print("reference:")
+print(np.array([["TN", "FP"], ["FN", "TP"]]))
+print()
+print("confusion [%s]:" % clf.__class__.__name__)
+print(confusion_matrix(test_samples["SERIOUS"], clf.predict(test_matrix)))
+print()
+print("confusion [%s]:" % baseline.__class__.__name__)
+print(confusion_matrix(test_samples["SERIOUS"], baseline.predict(test_matrix)))
 
--- assign5/topic.py
+++ assign5/topic.py
@@ -12,40 +12,112 @@ import gensim
 import nltk
 import string
 
+from nltk.stem.porter import PorterStemmer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.dummy import DummyClassifier
+from sklearn.metrics import confusion_matrix
+
 samples = pd.read_csv("serious.csv", encoding="latin1")
 samples = samples[samples["SYMPTOM_TEXT"].notnull()]
 
+NUM_TOPICS = 100
+
 # Partition the data into training/testing sets
-train_samples = samples.sample(100)
+train_samples = samples.sample(frac=0.5)
 test_samples = samples.drop(train_samples.index)
+og_test_symptoms = test_samples["SYMPTOM_TEXT"].copy()
+
+print("Train : {} ({:.2%})".format(
+    train_samples.shape[0],
+    train_samples.shape[0] / samples.shape[0]))
+print("Test  : {} ({:.2%})".format(
+    test_samples.shape[0],
+    test_samples.shape[0] / samples.shape[0]))
+
 
 # Filter stop words and punctuation
-stop_words = nltk.corpus.stopwords.words("english") +\
-             [sym for sym in string.punctuation]
+def tokenize(dataframe, column_label=""):
+    series = dataframe
+    if type(dataframe) != pd.Series:
+        series = dataframe[column_label]
 
-remove_stops = lambda sent: [token for token in
-                             nltk.word_tokenize(sent.lower())
-                             if token not in stop_words]
+    stop_words = nltk.corpus.stopwords.words("english") +\
+                 [sym for sym in string.punctuation]
 
-symptoms = train_samples["SYMPTOM_TEXT"].apply(remove_stops)
-train_samples["SYMPTOM_TEXT"] = symptoms
+    remove_stops = lambda sent: [token for token in
+                                 nltk.word_tokenize(sent.lower())
+                                 if token not in stop_words]
+    stemmer = PorterStemmer()
+    stem_tokens = lambda tokens: [stemmer.stem(token) for token in tokens]
+
+    return dataframe[column_label].apply(remove_stops).apply(stem_tokens)
+
+
+# Apply tokenization to training/testing sets
+print("Removing stop words/stemming")
+train_samples["SYMPTOM_TEXT"] = tokenize(train_samples, "SYMPTOM_TEXT")
+test_samples["SYMPTOM_TEXT"] = tokenize(test_samples, "SYMPTOM_TEXT")
+
 
 # Create LDA/LSI model for topic extraction
+print("Creating tf-idf models")
 dictionary = gensim.corpora.Dictionary(train_samples["SYMPTOM_TEXT"])
-corpus = [dictionary.doc2bow(text) for text in train_samples["SYMPTOM_TEXT"]]
-tfidf = gensim.models.TfidfModel(corpus)
-tcorpus = [tfidf[e] for e in corpus]
+train_corpus = [dictionary.doc2bow(text)
+                for text in train_samples["SYMPTOM_TEXT"]]
+test_corpus = [dictionary.doc2bow(text)
+               for text in test_samples["SYMPTOM_TEXT"]]
+tfidf = gensim.models.TfidfModel(train_corpus)
+train_tfidf_corpus = [tfidf[e] for e in train_corpus]
+test_tfidf_corpus = [tfidf[e] for e in test_corpus]
 
-model = gensim.models.lsimodel.LsiModel(corpus = tcorpus,
-                                        id2word = dictionary,
-                                        num_topics = 15)
+print("Creating LSI model")
+model = gensim.models.lsimodel.LsiModel(
+    corpus = train_tfidf_corpus,
+    id2word = dictionary,
+    num_topics = NUM_TOPICS)
 
-topic_words = [[w for w, _ in tups] for _, tups in
-               model.show_topics(formatted = 0, num_words = 10)]
+topic_words = [(index, [w for w, _ in tups]) for index, tups in
+               model.show_topics(num_topics = 300,
+                                 formatted = 0,
+                                 num_words = 10)]
 
+# Check out our dope topics
+print("Topics:")
 for topic in topic_words:
     print(topic)
 
+train_X = pd.DataFrame([dict(model[sample])
+                        for sample in train_tfidf_corpus],
+                       columns = pd.RangeIndex(NUM_TOPICS)).fillna(0)
+train_y = train_samples["SERIOUS"].reset_index(drop=True)
 
+test_X = pd.DataFrame([dict(model[sample])
+                       for sample in test_tfidf_corpus],
+                       columns = pd.RangeIndex(NUM_TOPICS)).fillna(0)
+test_y = test_samples["SERIOUS"].reset_index(drop=True)
+
+clf = RandomForestClassifier()
+
+print("Training a %s on the topic probability matrix" % clf.__class__.__name__)
+clf.fit(train_X, train_y)
+baseline = DummyClassifier(strategy="prior").fit(train_X, train_y)
+
+
+# Print classifier's accuracy on the test set
+print()
+print("score    :", clf.score(test_X, test_y))
+print("baseline :", baseline.score(test_X, test_y))
+print()
+print("~ confusion ~")
+print("reference:")
+print(np.array([["TN", "FP"], ["FN", "TP"]]))
+print()
+print("confusion [%s]:" % clf.__class__.__name__)
+print(confusion_matrix(test_y, clf.predict(test_X)))
+print()
+print("confusion [%s]:" % baseline.__class__.__name__)
+print(confusion_matrix(test_y, baseline.predict(test_X)))
 
 
